6e4e025b5e5eb1fb187c868906c8b276769dbbe7
davidduan04@gmail.com <davidduan04@gmail.com>
2023-10-30 18:17:13 -0400

    init main

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,4 +1,18 @@
 package app;
 
+import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
 public class Main {
+
+    public static void main(String[] args) {
+
+        //Main window
+        JFrame application = new JFrame("WellFit+");
+        application.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        application.setVisible(true);
+        application.setSize(800,400);
+
+
+    }
 }
