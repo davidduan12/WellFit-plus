05133eed2ecd40335970d7721fc8890c5bbd1731
Siyi Zhu <sophie20040402@gmail.com>
2023-11-04 21:31:41 -0400

    Adding AddFoodState. No problems on it now.

--- src/interface_adapter/AddFood/AddFoodState.java
+++ src/interface_adapter/AddFood/AddFoodState.java
@@ -1,4 +1,21 @@
 package interface_adapter.AddFood;
 
-public class AddState {
+import java.util.ArrayList;
+
+public class AddFoodState {
+    private ArrayList food = new ArrayList<>();
+
+    public AddFoodState(AddFoodState copy){
+        food = copy.food;
+    }
+
+    public AddFoodState(){}
+
+    public void setFood(ArrayList food){
+        this.food = food;
+    }
+
+    public ArrayList getFood(){
+        return food;
+    }
 }
