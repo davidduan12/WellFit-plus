e4114a55defe2f7035a00d43091716faf66c176b
Siyi Zhu <sophie20040402@gmail.com>
2023-11-04 21:30:37 -0400

    Adding Presenter. Having problem since we haven't import the OutputData

--- src/interface_adapter/AddFood/AddFoodPresenter.java
+++ src/interface_adapter/AddFood/AddFoodPresenter.java
@@ -1,4 +1,31 @@
 package interface_adapter.AddFood;
+import src.use_case.add_food.AddFoodOutputBoundary;
 
-public class AddPresenter {
+public class AddFoodPresenter implements AddFoodOutputBoundary {
+    private final AddFoodViewModel addFoodViewModel;
+
+    public AddFoodPresenter(AddFoodViewModel addFoodViewModel){
+        this.addFoodViewModel = addFoodViewModel;
+    }
+
+    public void prepareSuccessView(OutputData food){
+        AddFoodState addFoodState = addFoodViewModel.getState();
+        addFoodState.setFood(food.getname());
+        this.addFoodViewModel.setAddState(addFoodState);
+        addFoodViewModel.firePropertyChanged();
+    }
+
+    @Override
+    public void prepareFailView(OutputData outputData) {
+        // I think it should be String error in the parameter instead of Output Data.
+        // Should check it in AddFood OutputBoundary.
+    }
+
+    public void prepareFailView(String error){
+        AddFoodState addFoodState = addFoodViewModel.getState();
+        addFoodState.setFoodError(error);
+        addFoodViewModel.firePropertyChanged();
+    }
+
+
 }
