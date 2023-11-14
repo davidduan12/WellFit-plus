6103f846c663b781c34a50977b3fc7aa369ea8c2
Siyi Zhu <sophie20040402@outlook.com>
2023-11-14 16:46:16 -0500

    Change something detailed

--- src/interface_adapter/AddFood/AddFoodController.java
+++ src/interface_adapter/AddFood/AddFoodController.java
@@ -6,12 +6,12 @@ public class AddFoodController {
     final AddFoodInputBoundary addFoodUseCaseInteractor;
 
     public AddFoodController(AddFoodInputBoundary addFoodUseCaseInteractor) {
+
         this.addFoodUseCaseInteractor = addFoodUseCaseInteractor;
     }
 
     public void execute(String name, Double calorie){
         AddFoodInputData inputData = new AddFoodInputData(name);
-
         addFoodUseCaseInteractor.execute(inputData);
     }
 
