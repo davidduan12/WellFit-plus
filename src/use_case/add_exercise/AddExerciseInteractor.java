c53e3f01e8d88d2957dc20ab0b81ef954038fd7a
Siyi Zhu <sophie20040402@outlook.com>
2023-11-27 16:19:05 -0500

    The adding exercise use case

---
+++ src/use_case/add_exercise/AddExerciseInteractor.java
@@ -0,0 +1,24 @@
+package use_case.add_exercise;
+
+import use_case.UserDataAccessInterface;
+
+import java.util.ArrayList;
+
+public class AddExerciseInteractor implements AddExerciseInputBoundary{
+    final ExerciseAddDataAccessInterface exerciseAddDataAccessInterface;
+    final AddExerciseOutputBoundary addExerciseOutputBoundary;
+    final UserDataAccessInterface userDataAccessInterface;
+    public AddExerciseInteractor(ExerciseAddDataAccessInterface exerciseAddDataAccessInterface, AddExerciseOutputBoundary addExerciseOutputBoundary, UserDataAccessInterface userDataAccessInterface){
+        this.exerciseAddDataAccessInterface = exerciseAddDataAccessInterface;
+        this.addExerciseOutputBoundary = addExerciseOutputBoundary;
+        this.userDataAccessInterface = userDataAccessInterface;
+    }
+
+    @Override
+    public void execute(AddExerciseInputData inputData) {
+        String query = inputData.getName();
+        String apiData = exerciseAddDataAccessInterface.fetchDataFromNutritionix(query);
+        ArrayList<ArrayList<String>> records = userDataAccessInterface.readToCSV("/data/sample_user.csv");
+        userDataAccessInterface.writeToCSV(records);
+    }
+}
