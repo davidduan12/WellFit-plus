dd32b09a291707dfb9b01feff03e7463757c5dc5
William Wu <williamleyiwu@gmail.com>
2023-12-04 22:09:11 -0500

    .idea

--- src/data_access/FileUserDataAccessObject.java
+++ src/data_access/FileUserDataAccessObject.java
@@ -1,18 +1,16 @@
 package data_access;
+import entity.User;
 import use_case.UserDataAccessInterface;
 
+import java.io.*;
+import java.util.*;
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.Collection;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.util.ArrayList;
 
 public class FileUserDataAccessObject implements UserDataAccessInterface {
     private String filepath;
+
+    private final Map<String, User> accounts = new HashMap<>();
+
     public FileUserDataAccessObject(String filepath){
         this.filepath = filepath;
     }
@@ -44,6 +42,102 @@ public class FileUserDataAccessObject implements UserDataAccessInterface {
             e.printStackTrace();
         }
         return records;
+    }
+
+    //sign up
+    public void userWriting(User user){
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(filepath))) {
+            writer.newLine();
+            writer.write(user.getUsername() + "," + user.getPassword() + "," + user.getHeight() + "," + user.getHeight() + "," + ",");
+            accounts.put(user.getUsername(), user);
+            }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Return whether a user exists with username identifier.
+     * @param identifier the username to check.
+     * @return whether a user exists with username identifier
+     */
+
+    public boolean existsByName(String identifier) {
+        return accounts.containsKey(identifier);
+    }
+
+
+    //sign up
+    public boolean userLogin(String targetUsername, String password){
+        try (BufferedReader reader = new BufferedReader(new FileReader(filepath))) {
+            reader.readLine();
+            String line;
+            while ((line = reader.readLine()) != null){
+                String[] userData = line.split(",");
+                if (userData[0].equals(targetUsername)){
+                    if (userData[1].equals(password)){
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public void writeExerciseCaloriesToCSV(Map<String, String> exerciseData, String username) {
+        List<String> lines = new ArrayList<>();
+        String line;
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(filepath))) {
+            reader.readLine();
+
+            while ((line = reader.readLine()) != null){
+                String[] userData = line.split(",");
+                if (userData[0].equals(username)){
+                    userData[5] = userData[5] + exerciseData.toString();
+                }
+                lines.add(String.join(",", userData));
+            }
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(filepath))) {
+                for (String updatedLine : lines) {
+                    writer.write(updatedLine);
+                    writer.newLine();
+                }
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void writeFoodCaloriesToCSV(Map<String, String> foodData, String username) {
+        List<String> lines = new ArrayList<>();
+        String line;
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(filepath))) {
+            reader.readLine();
+
+            while ((line = reader.readLine()) != null){
+                String[] userData = line.split(",");
+                if (userData[0].equals(username)){
+                    userData[4] = userData[4] + foodData.toString();
+                }
+                lines.add(String.join(",", userData));
+            }
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(filepath))) {
+                for (String updatedLine : lines) {
+                    writer.write(updatedLine);
+                    writer.newLine();
+                }
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
