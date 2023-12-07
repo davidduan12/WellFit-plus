f19af4e8e733a9f97cda16ad4f5ec099adc42426
shaoyuq3 <yuqing.shao@mail.utoronto.ca>
2023-12-07 16:23:27 -0500

    Update For Test again and again

---
+++ test/interface_adapter/LoggedIn/AddExercise/AddExercisePresenterTest.java
@@ -0,0 +1,16 @@
+package interface_adapter.LoggedIn.AddExercise;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AddExercisePresenterTest {
+
+    @Test
+    void prepareSuccessView() {
+    }
+
+    @Test
+    void prepareFailView() {
+    }
+}
---
+++ test/interface_adapter/LoggedIn/AddExercise/AddExerciseStateTest.java
@@ -0,0 +1,28 @@
+package interface_adapter.LoggedIn.AddExercise;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AddExerciseStateTest {
+
+    @Test
+    void getExerciseName() {
+    }
+
+    @Test
+    void setExerciseName() {
+    }
+
+    @Test
+    void getExerciseLength() {
+    }
+
+    @Test
+    void setExerciseLength() {
+    }
+
+    @Test
+    void setExerciseError() {
+    }
+}
