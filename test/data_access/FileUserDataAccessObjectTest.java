dc2b304c714349ac654f6f1badc66bbaa5fd1bf8
William Wu <williamleyiwu@gmail.com>
2023-12-06 23:22:49 -0500

    bug fixes

--- test/data_access/FileUserDataAccessObjectTest.java
+++ test/data_access/FileUserDataAccessObjectTest.java
@@ -3,18 +3,70 @@ package data_access;
 import entity.User;
 import entity.UserFactory;
 
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class FileUserDataAccessObjectTest {
+        public static void main(String[] args) throws IOException {
+            UserFactory userFactory = new UserFactory();
+            FileUserDataAccessObject fileUserDataAccessObject = new FileUserDataAccessObject("./data/test_user.csv", userFactory);
+            User user = new User("tmd", "Ihateschool", 199, 99);
+            fileUserDataAccessObject.userWriting(user);
+            System.out.println(fileUserDataAccessObject.userLogin("tmd", "Ihateschool"));
+            System.out.println(fileUserDataAccessObject.userLogin("tmd", "Iloveschool"));
+            Map<String, String> ex = new HashMap<>();
+            ex.put("running", "100");
+            ex.put("punching", "100");
+            Map<String, String> fd = new HashMap<>();
+            fd.put("munch", "120");
+            fd.put("lunch", "200");
+            fileUserDataAccessObject.writeFoodCaloriesToCSV(fd, "tmd");
+            fileUserDataAccessObject.writeExerciseCaloriesToCSV(ex, "tmd");
 
-    public static void main(String[] args) throws IOException {
-        UserFactory userFactory = new UserFactory();
-        FileUserDataAccessObject fileUserDataAccessObject = new FileUserDataAccessObject("./data/test_user.csv", userFactory);
-        User user = new User("tmd", "Ihateschool", 199, 99);
-        fileUserDataAccessObject.userWriting(user);
-    }
+////        fileUserDataAccessObject.writeFoodCaloriesToCSV(fd, "tmd");
+//        System.out.println(fdarr[0]);
+//        System.out.println(fdarr[1]);
+//        System.out.println();
+//        System.out.println(fdarr[0].substring(fdarr[0].indexOf("=")+1));
+//        System.out.println(fdarr[1].substring(fdarr[1].indexOf("=")+1, fdarr[1].indexOf("}")));
+//        System.out.println();
+//        update("./data/test_user.csv");
+
+
+        }
+
+
+        public static void update(String filepath) throws FileNotFoundException {
+            List<String> lines = new ArrayList<>();
+            String line;
+            try (BufferedReader reader = new BufferedReader(new FileReader(filepath))){
+                while ((line = reader.readLine()) != null) {
+                    lines.add(line);
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(filepath))) {
+                for (String updatedLine : lines) {
+                    writer.write(updatedLine);
+                    writer.newLine();
+                }
+                writer.write("hs");
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            for (String str : lines){
+                System.out.println(str);
+            }
+        }
+
+
 
 
     @org.junit.jupiter.api.Test
